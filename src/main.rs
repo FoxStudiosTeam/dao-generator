3e39a630137d5cbabdd2aeb7b44ce0da52584202
kaiva-morphin <kaivamorphin@gmail.com>
2025-09-03 21:47:29 +0300

    up to latest version of schema_reader(rust_commons)

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use anyhow::Result;
 use clap::Parser;
 use std::{fs, path::{Path, PathBuf}};
 
-use rust_commons::schema_reader::prelude::*;
+use schema_reader::prelude::*;
  
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let args = Args::parse();
 
     let schema = Schema::from_dir(args.config_folder)?;
+
+    let mut reg = handlebars::Handlebars::new();
+    reg.register_schema_reader_helpers();
 
     if args.template_path.is_dir() {
         let dir = fs::read_dir(args.template_path)?;
@@ -18,14 +21,17 @@ fn main() -> Result<()> {
             let stem = entry.path().file_stem().unwrap().to_str().unwrap().to_owned();
             fs::create_dir(Path::new(&format!("{}/{}", args.target_path, stem))).ok();
             let template = entry.path();
-            let res = schema.render_tables(&template)?;
+            reg.register_template_file(&stem, &template)?;
+            let res = schema.render_tables(&reg, &stem)?;
             for (table_name, rendered) in res {
                 fs::write(Path::new(&format!("{}/{}/{}.rs", args.target_path, stem, table_name)), rendered)?;
             }
         }
     } else {
         let template = args.template_path;
-        let res = schema.render_tables(&template)?;
+        let stem = template.file_stem().unwrap().to_str().unwrap().to_owned();
+        reg.register_template_file(&stem, &template)?;
+        let res = schema.render_tables(&reg, &stem)?;
         for (table_name, rendered) in res {
             fs::write(Path::new(&format!("{}/{}.rs", args.target_path, table_name)), rendered)?;
         }
